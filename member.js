7b298e5d10744357a414d06f4d7793ed35303c69
Margaret Mumbi Gichuhi <39958354+meggmumbi@users.noreply.github.com>
2023-11-21 12:21:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillMember(){
+    var member = [
+        {name: "Kadek", age: 20, hobby: "Music"},
+        {name: "Dewi", age: 25, hobby: "Traveling"},
+        {name: "Sinta", age: 22, hobby: "Reading"}
+    ]
+    return member
+}
